7991fb0442e37cdacabd0f8fc8859cf7154bfa5a
Patrick Martin <patrickmmartin@gmail.com>
2016-05-05 23:40:20 +0100

    factor out hard-coded bees - closes #2

--- Two Bees.py
+++ Two Bees.py
@@ -20,10 +20,15 @@ def showxy(event):
 c.bind('<Motion>',showxy)
 
 bee_custome_list=[]
+bee_list=[]
 pos=0,0
 shift_x=20
 x=50
 y=170
+
+def bees():
+    for bindex in range(0, 3):
+        bee_list.append((0, 100 * bindex));
 
 def bee_costumes():
 
@@ -52,9 +57,9 @@ def do_animation(currentframe):
         c.after(1)
 
     c.delete('current_bee')
-    do_image(x,shift_x, (0, 0))
-    do_image(x,shift_x, (0, 100))
-    do_image(x,shift_x, (0, 200))
+
+    for bee in bee_list:
+        do_image(x, shift_x, bee)
 
     if x>650:
         shift_x=-20
@@ -69,6 +74,7 @@ def do_animation(currentframe):
     c.after(300,do_animation,currentframe)
 
 bee_costumes()
+bees()
 
 c.after(1,do_animation,0)
 
