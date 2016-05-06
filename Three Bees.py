3340812e790a48e73d474f61d700bd882aff299b
Patrick Martin <patrickmmartin@gmail.com>
2016-05-06 21:03:43 +0100

    resolves #4

--- Three Bees.py
+++ Three Bees.py
@@ -16,13 +16,36 @@ class BeeApp:
         self.canvas.pack()
 
 class Bee:
-    " TODO(PMM) this class represents a bee sprite "
     def __init__(self, offset):
         self.offset = offset
         # TODO(PMM) there is clearly scope for more properties in future iterations
 
 class Swarm:
     " TODO(PMM) this class manages a swarm of Bees "
+
+    def bees(self):
+        result = []
+
+        for bindex in range(0, 3):
+            result.append(Bee((0, 100 * bindex)));
+        return result
+
+    def bee_costumes(self):
+
+        result = []
+        image_list = ['bee1.gif', 'bee2.gif', 'bee3.gif', 'bee4.gif']
+
+        for image in image_list:
+            bee_costume = PhotoImage(file = image)
+            bee_sample = bee_costume.subsample(5,5)
+            result.append(bee_sample)
+
+        return result
+
+    def __init__(self):
+        self.custome_list = self.bee_costumes()
+        self.list = self.bees()
+
 
 # not really needed at this time
 #def showxy(event):
@@ -35,43 +58,17 @@ class Swarm:
 pos=0,0
 shift_x=20
 x=50
-y=170
-
-def bees():
-    result = []
-
-    for bindex in range(0, 3):
-        result.append(Bee((0, 100 * bindex)));
-
-    return result
-
-def bee_costumes():
-
-    result = []
-    image_list = ['bee1.gif', 'bee2.gif', 'bee3.gif', 'bee4.gif']
-
-    for image in image_list:
-        bee_costume = PhotoImage(file = image)
-        bee_sample = bee_costume.subsample(5,5)
-        result.append(bee_sample)
-
-    return result
 
 
 # construct here
 app = BeeApp()
 
-# set up costumes
-bee_custome_list = bee_costumes()
-
-# set up bee list
-bee_list = bees()
-
+# construct here
+swarm = Swarm()
 
 def do_animation(currentframe):
     global x
     global shift_x
-    global c
     global pos
     
     def do_image(x, shift_x, offset):
@@ -79,27 +76,28 @@ def do_animation(currentframe):
         x_offset, y_offset = offset
         # TODO(PMM) - this expression should derive from the costumes
         frameIndex = (currentframe % 2) + (2 if (shift_x < 0) else 0)
-        app.canvas.create_image(50 + x + x_offset, 170 + y_offset, image=bee_custome_list[frameIndex], tag='current_bee')
-        app.canvas.move('current_bee',shift_x,0)
+        app.canvas.create_image(50 + x + x_offset, 170 + y_offset,
+                                image = swarm.custome_list[frameIndex],
+                                tag='current_bee')
         app.canvas.update()
         app.canvas.after(1)
 
     app.canvas.delete('current_bee')
 
-    for bee in bee_list:
+    for bee in swarm.list:
         do_image(x, shift_x, bee.offset)
 
-    if x>650:
-        shift_x=-20
+    if x > 650:
+        shift_x = -20
 
-    if x<20:
-        shift_x=20
+    if x < 20:
+        shift_x = 20
 
-    x+=shift_x
+    x += shift_x
 
-    currentframe = currentframe+1
+    currentframe = currentframe + 1
 
-    app.canvas.after(300,do_animation,currentframe)
+    app.canvas.after(100, do_animation,currentframe)
 
 
 app.canvas.after(1,do_animation,0)
